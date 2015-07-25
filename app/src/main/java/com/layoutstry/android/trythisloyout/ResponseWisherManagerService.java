8bf8522fcd49bcd6fdc40e44b0edc3f711750664
Parth <parthmodi54@yahoo.com>
2015-07-25 12:53:44 +0530

    -'Swipe to Refresh' functionality added

---
+++ app/src/main/java/com/layoutstry/android/trythisloyout/ResponseWisherManagerService.java
@@ -0,0 +1,16 @@
+package com.layoutstry.android.trythisloyout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by User on 23-07-2015.
+ */
+// Broadcast receiver for receiving status updates from the IntentService
+public class ResponseWisherManagerService extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
